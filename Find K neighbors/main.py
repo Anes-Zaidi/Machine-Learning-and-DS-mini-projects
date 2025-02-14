ae66a2491105f092e4fe520c5e424f31569460c8
Zaidi Anes Nasrallah <141640698+Anas66Az@users.noreply.github.com>
2025-02-14 12:05:16 +0100

    Added main.py

---
+++ Find K neighbors/main.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+
+points = [(random.randint(0, 100), random.randint(0, 100)) for _ in range(10)]
+k = 3
+
+def kNearestNighboor(points , k):
+    points = np.array(points)
+    n = len(points)
+    results = []
+    for i in range(n):
+        distances = np.sqrt(np.sum((points[i] - points) ** 2 , axis = 1))
+        nearestDist = np.partition(distances , k)[:k]
+        nearstPointsIndcs = np.argpartition(distances , k)[:k]
+        results.append(points[nearstPointsIndcs])
+    return results
+
+x ,y = zip(*points)
+neighbArr = kNearestNighboor(points , k)
+neighbors =  [tuple(point) for sublist in neighbArr for point in sublist]
+i , j  = zip(*neighbors)
+
+plt.style.use('ggplot') 
+plt.scatter(x , y , s=100)
+plt.plot(i , j , color ="black")
+
+plt.show()
